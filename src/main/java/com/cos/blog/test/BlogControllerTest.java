68a4148738baab64c1963258a7c9b2c42e80a8aa
dhkwak7791 <kkddhh1998@gmail.com>
2021-08-24 16:53:39 +0900

    setting accept

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,16 @@
+package com.cos.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller //스프링이 com.cos.blog 패키지 이하를 스캔해서 모든 파일을 메모리에 new 하진 않음
+// 특정 어노테이션이 붙어있는 클래스 파일들을 new해서 스프링 컨테이너에 관리해줍니다.
+@RestController
+public class BlogControllerTest { 
+	@GetMapping("/test/hello")
+	public String Hello(){
+		return "<h1>hello spring boot?</h1>";
+	}
+
+}
