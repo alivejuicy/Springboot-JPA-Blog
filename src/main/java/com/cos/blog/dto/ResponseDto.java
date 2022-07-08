19af90632ff9eb1480320a230796393a7cca9c72
alivejuicy <kkddhh1998@gmail.com>
2022-07-08 14:01:53 +0900

    [feat] responseDto 생성

--- src/main/java/com/cos/blog/dto/ResponseDto.java
+++ src/main/java/com/cos/blog/dto/ResponseDto.java
@@ -1,5 +1,18 @@
 package com.cos.blog.dto;
 
-public class ResponseDto {
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResponseDto<T> { //제너릭 생성
+	HttpStatus status;
+	T data;
 
 }
