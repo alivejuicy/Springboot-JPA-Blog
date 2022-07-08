9b0fa53066389f0a7915078edc284d8e2901982d
alivejuicy <kkddhh1998@gmail.com>
2022-07-08 14:00:25 +0900

    [refactor] responseDto 적용

--- src/main/java/com/cos/blog/controller/UserApiController.java
+++ src/main/java/com/cos/blog/controller/UserApiController.java
@@ -1,18 +1,20 @@
 package com.cos.blog.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.entity.User;
 
 @RestController
 public class UserApiController {
 	
 	@PostMapping("/api/user")
-	public int save(@RequestBody User user) {
+	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("save 함수 호출");
-		return 1;
+		return new ResponseDto<Integer>(HttpStatus.OK,1);
 	}
 
 }
