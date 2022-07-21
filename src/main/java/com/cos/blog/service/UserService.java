a2305a3ecb96d6e5b5b61a79cb7ecee96e7e125d
alivejuicy <kkddhh1998@gmail.com>
2022-07-21 15:39:53 +0900

    login servie 구현

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,25 +1,26 @@
 package com.cos.blog.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cos.blog.entity.User;
 import com.cos.blog.repository.UserRepository;
 
+@Service
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
 	
-	public int signUp(User user){
-		try {
-			 userRepository.save(user);
-			 return 1;
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("UserService : 회원가입"+e.getMessage());
-			// TODO: handle exception
-		} 
-		return -1;
-		
+	@Transactional
+	public void signUp(User user){
+		userRepository.save(user);
+	}
+	
+	@Transactional(readOnly = true)
+	public User signIn(User user) {
+		return userRepository.findByUsernameAndPassword(user.getUsername(),user.getPassword());
 	}
 }
