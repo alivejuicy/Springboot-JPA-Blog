0967a4f43f850968e1c6e9076d45ea5e4827b4b7
alivejuicy <kkddhh1998@gmail.com>
2022-07-24 23:11:54 +0900

    update 메소드

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,9 +3,6 @@ let index = {
 			$("#btn-save").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해
 				this.save();
 			});
-			$("#btn-login").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해
-				this.login();
-			});
 			$("#btn-update").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해
 				this.update();
 			});
@@ -26,8 +23,7 @@ let index = {
 			// ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환?
 			$.ajax({ 
 				type: "POST",
-				//url: "/auth/joinProc",
-				url:"/api/user",
+				url: "/auth/joinProc",
 				data: JSON.stringify(data), // http body데이터
 				contentType: "application/json; charset=utf-8",// body데이터가 어떤 타입인지(MIME)
 				dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript오브젝트로 변경
@@ -44,39 +40,7 @@ let index = {
 			}); 
 			
 		},
-		login: function(){
-			//alert('user의 save함수 호출됨');
-			let data = {
-					username: $("#username").val(),
-					password: $("#password").val()
-					
-			};
-			
-			//console.log(data);
-			
-			// ajax호출시 default가 비동기 호출
-			// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
-			// ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환?
-			$.ajax({ 
-				type: "POST",
-				//url: "/auth/joinProc",
-				url:"/api/user/login",
-				data: JSON.stringify(data), // http body데이터
-				contentType: "application/json; charset=utf-8",// body데이터가 어떤 타입인지(MIME)
-				dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript오브젝트로 변경
-			}).done(function(resp){
-				if(resp.status === 500){
-					alert("로그인에 실패하였습니다.");
-				}else{
-					alert("로그인이 완료되었습니다.");
-					location.href = "/";
-				}
-
-			}).fail(function(error){
-				alert(JSON.stringify(error));
-			}); 
-			
-		},
+		
 		/*
 		update: function(){
 			//alert('user의 save함수 호출됨');
