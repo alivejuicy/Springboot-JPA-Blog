5987133e2b83c5fffcdc1469c48607fc388d9612
alivejuicy <kkddhh1998@gmail.com>
2022-07-24 22:52:22 +0900

    [feat] 패스워드 해쉬 암호화 적용

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -3,8 +3,10 @@ package com.cos.blog.service;
 import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.cos.blog.entity.RoleType;
 import com.cos.blog.entity.User;
 import com.cos.blog.repository.UserRepository;
 
@@ -14,8 +16,15 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 	@Transactional
 	public void signUp(User user){
+		String rawPassword = user.getPassword(); //패스워드 원문
+		String encPassword = encoder.encode(rawPassword); //패스워드 해쉬화
+		user.setPassword(encPassword); //패스워드 해쉬화 setting
+		user.setRole(RoleType.USER); 
 		userRepository.save(user);
 	}
 	
