33074f734387898940d5ce7f993a66b6c5d1ae9a
alivejuicy <kkddhh1998@gmail.com>
2022-07-24 21:44:01 +0900

    [feat] andMatchers 컨텍스트 추가, csrf 토큰 비활성화

--- src/main/java/com/cos/blog/configuration/SecurityConfig.java
+++ src/main/java/com/cos/blog/configuration/SecurityConfig.java
@@ -1,21 +1,30 @@
 package com.cos.blog.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity //Fillter 등록
 @EnableGlobalMethodSecurity(prePostEnabled = true) //특정 주소로 접근을 하면 권한 및 인증을 미리 체크
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Bean
+	public BCryptPasswordEncoder encodePWD() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable()
 			.authorizeRequests() //요청에 대한 인가가 들어오면
-				.antMatchers("/auth/**") 
+				.antMatchers("/","/auth/**","/js/**","/css/**", "/image/**", "/dummy/**") 
 				.permitAll()
 				.anyRequest()
 				.authenticated()
