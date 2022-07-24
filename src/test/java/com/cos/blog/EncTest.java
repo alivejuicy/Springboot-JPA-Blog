5a96eea69186b6dfecc568271023a48fd9c4a88b
alivejuicy <kkddhh1998@gmail.com>
2022-07-24 22:52:52 +0900

    해시 암호화 TDD

---
+++ src/test/java/com/cos/blog/EncTest.java
@@ -0,0 +1,15 @@
+package com.cos.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class EncTest {
+	
+	@Test
+	public void hash_decode() {
+		String encPassword = new BCryptPasswordEncoder().encode("1234");
+		System.out.println("1234 해쉬 :"+encPassword);
+		
+	}
+
+}
