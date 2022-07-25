13cfb4f34ecf774b1bcb85eeccb7beecdbe2c678
alivejuicy <kkddhh1998@gmail.com>
2022-07-25 19:10:22 +0900

    findByUsername 메소드

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,6 +12,7 @@ import com.cos.blog.entity.User;
 //@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+	Optional<User> findByUsername(String username);
 }
 //1. JPA Naming 방식
 // User findByUsernameAndPassword(String username, String password);
